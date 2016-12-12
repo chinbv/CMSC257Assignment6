071c1000d55e888c4d95b4f4b85f217bfc1bff1c
Brandon Chin <icefence@me.com>
2016-12-11 19:57:15 -0500

    Updated

--- batchShell.sh
+++ batchShell.sh
@@ -20,13 +20,14 @@ echo "Least repetitive word: "
 cat $FILE | tr '[:punct:] [:space:]' ' ' | tr 'A-Z' 'a-z' | tr -s ' ' | tr ' ' '\n' | sort | uniq -c | sort -n | head -1
 
 echo "Number of Words that Start and End with \"D\" or \"d\""
-egrep -i '\<d' $FILE | egrep -i 'd\>' | wc -w
+# egrep -io '\<d' $FILE | egrep -io 'd\>' | wc -w
+
+egrep -io '\<d[^ ]*' $FILE | egrep -io 'd\>' | wc -w
 
 echo "Number of Words that Start with \"A\" or \"a\""
-egrep -i '\<a' $FILE | wc -w
+egrep -io '\<a' $FILE | wc -w
 
 echo "Number of numeric words"
-# egrep '/<[0-9]' $FILE | wc -w
 egrep -o '\b[0-9]+\b' $FILE | wc -w
 
 echo "Number of alphanumerical words"
